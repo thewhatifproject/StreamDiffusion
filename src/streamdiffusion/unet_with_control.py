cd0ca8536b4a083ad82b8493fcbe01045e012192
The 'What If' Project <contact.thewhatifproject@gmail.com>
2025-03-12 13:11:30 +0100

    dddd

--- src/streamdiffusion/unet_with_control.py
+++ src/streamdiffusion/unet_with_control.py
@@ -9,7 +9,14 @@ class UNet2DConditionControlNetModel(torch.nn.Module):
         self.controlnets = [controlnet.to(unet.device, dtype=unet.dtype) for controlnet in controlnets]
         self.controlnet_scales = controlnet_scales
 
-    def forward(self, sample, timestep, encoder_hidden_states, controlnet_images, **kwargs) -> torch.Tensor:
+    def forward(self, *args, **kwargs) -> torch.Tensor:
+        # Supponiamo che gli argomenti vengano passati in ordine:
+        # sample, timestep, encoder_hidden_states, controlnet_images
+        sample = args[0]
+        timestep = args[1]
+        encoder_hidden_states = args[2] if len(args) > 2 else kwargs.get("encoder_hidden_states")
+        controlnet_images = args[3] if len(args) > 3 else kwargs.get("controlnet_images")
+
         for i in range(len(self.controlnets)):
             down_samples, mid_sample = self.controlnets[i](
                 sample,
@@ -20,17 +27,13 @@ class UNet2DConditionControlNetModel(torch.nn.Module):
                 return_dict=False,
                 **kwargs
             )
-
             down_samples = [down_sample * self.controlnet_scales[i] for down_sample in down_samples]
             mid_sample *= self.controlnet_scales[i]
-
-            # merge samples
             if i == 0:
                 down_block_res_samples, mid_block_res_sample = down_samples, mid_sample
             else:
                 down_block_res_samples = [
-                    samples_prev + samples_curr
-                    for samples_prev, samples_curr in zip(down_block_res_samples, down_samples)
+                    prev + curr for prev, curr in zip(down_block_res_samples, down_samples)
                 ]
                 mid_block_res_sample += mid_sample
 
