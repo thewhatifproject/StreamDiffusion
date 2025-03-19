3602c12ebc1a2c66ba23eba27d72455fa9eadda3
The 'What If' Project <contact.thewhatifproject@gmail.com>
2025-03-19 15:06:57 +0100

    dd

--- src/streamdiffusion/utils/wrapper.py
+++ src/streamdiffusion/utils/wrapper.py
@@ -302,7 +302,7 @@ class StreamDiffusionWrapper:
                 stream.controlnet = torch.compile(stream.controlnet, mode="reduce-overhead", fullgraph=True)
         
             stream.unet = autoquant(stream.vae, error_on_unseen=False)
-            stream.vae = autoquant(stream.vae, error_on_unseen=False)
+            #stream.vae = autoquant(stream.vae, error_on_unseen=False)
 
         if seed < 0:  # Random seed
             seed = np.random.randint(0, 1000000)
