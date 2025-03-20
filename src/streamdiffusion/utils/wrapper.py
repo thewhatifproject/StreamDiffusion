b6242d87075c8ea4d8b35257cf0c81e5435bdf26
The 'What If' Project <contact.thewhatifproject@gmail.com>
2025-03-20 09:58:54 +0100

    dd

--- src/streamdiffusion/utils/wrapper.py
+++ src/streamdiffusion/utils/wrapper.py
@@ -286,19 +286,19 @@ class StreamDiffusionWrapper:
         if acceleration and pipe is not None:
 
             print("Memory format conversion...")
-            stream.unet.to(memory_format=torch.channels_last)
-            stream.vae.to(memory_format=torch.channels_last)
+            #stream.unet.to(memory_format=torch.channels_last)
+            #stream.vae.to(memory_format=torch.channels_last)
             #stream.text_encoder.to(memory_format=torch.channels_last)
-            if self.is_controlnet_enabled:
-                stream.controlnet.to(memory_format=torch.channels_last)
+            #if self.is_controlnet_enabled:
+            #    stream.controlnet.to(memory_format=torch.channels_last)
             
             print("Apply torch compile optimization...")
-            stream.unet = torch.compile(stream.unet, mode="reduce-overhead", fullgraph=True)
-            stream.vae.decode = torch.compile(stream.vae.decode, mode="reduce-overhead", fullgraph=True)
+            #stream.unet = torch.compile(stream.unet, mode="reduce-overhead", fullgraph=True)
+            #stream.vae.decode = torch.compile(stream.vae.decode, mode="reduce-overhead", fullgraph=True)
             #stream.vae.encode = torch.compile(stream.vae.encode, mode="reduce-overhead", fullgraph=True)
             #stream.text_encoder = torch.compile(stream.text_encoder, mode="reduce-overhead", fullgraph=True)
-            if self.is_controlnet_enabled:
-                stream.controlnet = torch.compile(stream.controlnet, mode="reduce-overhead", fullgraph=True)
+            #if self.is_controlnet_enabled:
+                #stream.controlnet = torch.compile(stream.controlnet, mode="reduce-overhead", fullgraph=True)
 
         if seed < 0:  # Random seed
             seed = np.random.randint(0, 1000000)
