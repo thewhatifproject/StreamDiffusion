a9dc457f4eb5cf0cd1ba6215a7c20d39897c7bed
The 'What If' Project <contact.thewhatifproject@gmail.com>
2025-03-23 11:36:39 +0100

    sdxl

--- src/streamdiffusion/utils/wrapper.py
+++ src/streamdiffusion/utils/wrapper.py
@@ -39,7 +39,8 @@ class StreamDiffusionWrapper:
         similar_image_filter_max_skip_frame: int = 10,
         cfg_type: Literal["none", "full", "self", "initialize"] = "self",
         seed: int = 2,
-        use_safety_checker: bool = False
+        use_safety_checker: bool = False,
+        sdxl: bool = False
     ):
         self.sd_turbo = "turbo" in model_id_or_path
         self.device = device
@@ -66,7 +67,8 @@ class StreamDiffusionWrapper:
             CM_lora_type=CM_lora_type,
             use_tiny_vae=use_tiny_vae,
             cfg_type=cfg_type,
-            seed=seed
+            seed=seed,
+            sdxl=sdxl
         )
         
         #Eliminare se gira cosi
