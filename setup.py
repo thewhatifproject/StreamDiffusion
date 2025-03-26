319ca737a706ba015e86cbace106272c5e3cf516
The 'What If' Project <contact.thewhatifproject@gmail.com>
2025-03-26 10:24:31 +0100

    cnet

--- setup.py
+++ setup.py
@@ -2,37 +2,21 @@ import re
 
 from setuptools import find_packages, setup
 
-
 _deps = [
     "torch",
-    "xformers",
-    "diffusers==0.24.0",
+    "diffusers",
     "transformers",
     "accelerate",
     "fire",
     "omegaconf",
-    "cuda-python",
-    "onnx==1.15.0",
-    "onnxruntime==1.16.3",
-    "protobuf==3.20.2",
-    "colored",
     "pywin32;sys_platform == 'win32'",
     "controlnet-aux==0.0.9",
+    "huggingface_hub",
+    "numpy",
+    "peft"
 ]
 
 deps = {b: a for a, b in (re.findall(r"^(([^!=<>~]+)(?:[!=<>~].*)?$)", x)[0] for x in _deps)}
-
-
-def deps_list(*pkgs):
-    return [deps[pkg] for pkg in pkgs]
-
-
-extras = {}
-extras["xformers"] = deps_list("xformers")
-extras["torch"] = deps_list("torch", "accelerate")
-extras["tensorrt"] = deps_list("protobuf", "cuda-python", "onnx", "onnxruntime", "colored")
-
-extras["dev"] = extras["xformers"] + extras["torch"] + extras["tensorrt"]
 
 install_requires = [
     deps["fire"],
@@ -40,11 +24,15 @@ install_requires = [
     deps["diffusers"],
     deps["transformers"],
     deps["accelerate"],
+    deps["controlnet-aux"],
+    deps["huggingface_hub"],
+    deps["numpy"],
+    deps["peft"]
 ]
 
 setup(
     name="streamdiffusion",
-    version="0.1.1",
+    version="0.1.0",
     description="real-time interactive image generation pipeline",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
@@ -58,6 +46,5 @@ setup(
     package_data={"streamdiffusion": ["py.typed"]},
     include_package_data=True,
     python_requires=">=3.10.0",
-    install_requires=list(install_requires),
-    extras_require=extras,
+    install_requires=list(install_requires)
 )
