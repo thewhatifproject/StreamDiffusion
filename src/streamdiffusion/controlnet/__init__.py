d355a835fc78b648573de2c60d620ab6149e29c2
RyanOnTheInside <7623207+ryanontheinside@users.noreply.github.com>
2025-08-09 09:33:04 -0400

    remove legacy code
    replaced by module

--- src/streamdiffusion/controlnet/__init__.py
+++
@@ -1,2 +0,0 @@
-# Deprecated during refactor: legacy ControlNet pipelines are no longer used
-__all__ = []
--- src/streamdiffusion/ipadapter/__init__.py
+++
@@ -1,2 +0,0 @@
-# Deprecated during refactor: legacy IPAdapter pipeline is no longer used
-__all__ = []
